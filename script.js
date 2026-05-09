780b9fd5d0a71d74cbe2497ff2e9b6a88e437e1d
Bishnu sharma <bishnusharmafrommahuliya@gmail.com>
2026-05-08 23:54:49 -0300

    portfolio upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// 
+function toggleHire() {
+    const box = document.getElementById("hireBox");
+    const btn = document.getElementById("hireBtn");
+
+    box.classList.toggle("show");
+    btn.classList.toggle("active");
+}
